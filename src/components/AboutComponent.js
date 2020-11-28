d5a092ce774d78ba874ace8f04a44c1f5524bc76
anvil-ideation <landon@anvilideation.com>
2020-11-28 09:20:11 -0700

    Week 3 Workshop Assignment Tasks 2 & 3

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -7,9 +7,28 @@ function About(props) {
 
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+            <Media tag="li" key={partner.id}>
+                <RenderPartner partner={partner} />
+            </Media>
         );
     });
+
+    function RenderPartner({partner}) {
+        if (partner) {
+            return (
+                <>
+                    <Media object src={partner.image} alt={partner.name} width="150" />
+                    <Media body className="ml-5 mb-4">
+                        <Media heading>{partner.name}</Media>
+                        {partner.description}
+                    </Media>
+                </>
+            );
+        };
+        return (
+            <div />
+        );
+    }
 
     return (
         <div className="container">
