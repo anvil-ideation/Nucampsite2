423063a92d76ba7c0a3e9fca06a558a549aa12b6
anvil-ideation <landon@anvilideation.com>
2020-12-10 22:58:26 -0700

    React Final Assignment minus Task 2

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -34,9 +34,9 @@ function Home(props) {
             <div className="row">
                 <div className="col-md m-1">
                 <RenderCard
-                        item={props.promotion}
-                        isLoading={props.promotionLoading}
-                        errMess={props.promotionErrMess}
+                        item={props.campsite}
+                        isLoading={props.campsitesLoading}
+                        errMess={props.campsitesErrMess}
                     />
                 </div>
                 <div className="col-md m-1">
@@ -47,7 +47,12 @@ function Home(props) {
                 />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
+                <RenderCard 
+                        item={props.partner}
+                        isLoading={props.partnersLoading}
+                        errMess={props.partnersErrMess} 
+                />
+                    
                 </div>
             </div>
         </div>
